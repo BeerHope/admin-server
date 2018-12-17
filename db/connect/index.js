9bd2221793e82e0efef943c6c5107cdd3ad4a842
suilingbao <suilingbao@weidian.com>
2018-12-17 16:20:16 +0800

    alert chactset

--- db/connect/index.js
+++ db/connect/index.js
@@ -4,27 +4,33 @@ const Sequelize = require('sequelize')
 
 // 创建数据库连接池
 let createSeqExample = (config) => {
-  return new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
-    dialect: 'mysql',
-    port: 3306,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    },
-    logging: (res) => {
-      global.infoLog.info('本次请求执行的sql----', res)
-    },
-    // 请参考 Querying - 查询 操作符 章节
-    operatorsAliases: false
-  })
+    return new Sequelize(config.database, config.username, config.password, {
+        host: config.host,
+        dialect: 'mysql',
+        port: 3306,
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+        dialectOptions: {
+            charset: "utf8mb4",
+            collate: "utf8mb4_unicode_ci",
+            supportBigNumbers: true,
+            bigNumberStrings: true
+        },
+        logging: (res) => {
+            global.infoLog.info('本次请求执行的sql----', res)
+        },
+        // 请参考 Querying - 查询 操作符 章节
+        operatorsAliases: false
+    })
 }
 const db = (app) => {
-  let config = checkConfig(app)
-  let blogSequelize = createSeqExample(config.default)
-  return {blogSequelize}
+    let config = checkConfig(app)
+    let blogSequelize = createSeqExample(config.default)
+    return {blogSequelize}
 }
 
 export default db
